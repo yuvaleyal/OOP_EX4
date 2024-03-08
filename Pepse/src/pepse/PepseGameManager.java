69c7c62defebb507ace88602aba96356a8344035
Yuval Eyal <uveyal@gmail.com>
2024-03-08 12:15:45 +0200

    sky

--- Pepse/src/pepse/PepseGameManager.java
+++ Pepse/src/pepse/PepseGameManager.java
@@ -1,9 +1,30 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.GameGUIComponent;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
+    public PepseGameManager() {
+        super();
+    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+            UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        gameObjects().addGameObject(sky, Layer.BACKGROUND);
+    }
+
     public static void main(String[] args) {
-        new PepseGameManager().run();
+        PepseGameManager gameManager = new PepseGameManager();
+        gameManager.run();
     }
 }
--- Pepse/src/pepse/world/Sky.java
+++ Pepse/src/pepse/world/Sky.java
@@ -1,5 +1,23 @@
 package pepse.world;
 
-public class Sky {
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.util.Vector2;
+import java.awt.Color;
 
+public class Sky {
+    private static final Color BASIC_SKY_COLOR = Color.decode("#80c6e5");
+    private static final String SKY_TAG = "sky";
+
+    private Sky() {
+    }
+
+    public static GameObject create(Vector2 windowDimentions) {
+        GameObject sky = new GameObject(Vector2.ZERO, windowDimentions,
+                new RectangleRenderable(BASIC_SKY_COLOR));
+        sky.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sky.setTag(SKY_TAG);
+        return sky;
+    }
 }
