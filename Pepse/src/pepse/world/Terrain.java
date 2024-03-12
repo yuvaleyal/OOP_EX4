753bc981c1078a31e217110aed04198917d9dd4c
guytrabelsi <guytra2205@gmail.com>
2024-03-12 10:02:17 +0200

    supplied mattirial and block

--- Pepse/src/pepse/world/Terrain.java
+++ Pepse/src/pepse/world/Terrain.java
@@ -1,5 +1,11 @@
 package pepse.world;
 
-public class Terrain {
+import danogl.util.Vector2;
 
+public class Terrain {
+    private final float groundHeightAtX0;
+    
+    public Terrain(Vector2 windowDimensions, int seed){
+            groundHeightAtX0 = (float) (windowDimensions.y()*(2.0/3.0));
+        }
 }
