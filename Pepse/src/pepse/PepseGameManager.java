3a3f2fee1a092bdffbff6171ad02a73b50b75417
guytrabelsi <guytra2205@gmail.com>
2024-03-12 18:35:29 +0200

    Night class working

--- Pepse/src/pepse/PepseGameManager.java
+++ Pepse/src/pepse/PepseGameManager.java
@@ -1,6 +1,7 @@
 package pepse;
 
 import java.util.List;
+import java.util.Random;
 
 import danogl.GameManager;
 import danogl.GameObject;
@@ -10,11 +11,14 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.util.Vector2;
 import pepse.world.Sky;
 import pepse.world.Block;
 import pepse.world.Terrain;
+import pepse.world.daynight.Night;
 
 public class PepseGameManager extends GameManager {
+    private final int CYCLELENGTH = 30;
     public PepseGameManager() {
         super();
     }
@@ -23,15 +27,18 @@ public class PepseGameManager extends GameManager {
     public void initializeGame(ImageReader imageReader, SoundReader soundReader,
             UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
-        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        Vector2 windowDimensions = windowController.getWindowDimensions();
+        GameObject sky = Sky.create(windowDimensions);
         gameObjects().addGameObject(sky, Layer.BACKGROUND);
-        Terrain terrain = new Terrain(windowController.getWindowDimensions(), 0);
+        int seed = (int)new Random().nextGaussian();
+        Terrain terrain = new Terrain(windowDimensions, seed);
         List<Block> list_of_blocks = terrain.createInRange
         (0, (int)windowController.getWindowDimensions().x());
         for (Block block : list_of_blocks) {
             gameObjects().addGameObject(block);
         }
-
+        GameObject night = Night.create(windowDimensions, CYCLELENGTH);
+        gameObjects().addGameObject(night, Layer.UI);
     }
 
     public static void main(String[] args) {
--- Pepse/src/pepse/world/daynight/Night.java
+++ Pepse/src/pepse/world/daynight/Night.java
@@ -1,5 +1,29 @@
 package pepse.world.daynight;
 
+import java.awt.Color;
+
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.components.Transition;
+import danogl.components.Transition.TransitionType;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+
 public class Night {
+    private static final String NIGHT_TAG = "night";
+    private static final Float MIDNIGHT_OPACITY = 0.5f;
+
+    public static GameObject create(Vector2 windowDimensions,float cycleLength){
+        Renderable renderable = new RectangleRenderable(Color.BLACK);
+        GameObject night = new GameObject(Vector2.ZERO, windowDimensions, renderable);
+        night.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        night.setTag(NIGHT_TAG);
+        Transition transition = new Transition<Float>(night, night.renderer()::setOpaqueness
+        , 0.0f, MIDNIGHT_OPACITY, Transition.CUBIC_INTERPOLATOR_FLOAT, cycleLength,
+         TransitionType.TRANSITION_BACK_AND_FORTH, null);
+        return night;
+    }
 
 }
