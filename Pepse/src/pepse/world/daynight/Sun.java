177119159f51352ed324089ce18b2b481905b614
Yuval Eyal <uveyal@gmail.com>
2024-03-14 17:12:46 +0200

    leaves movements 1

--- Pepse/src/pepse/world/daynight/Sun.java
+++ Pepse/src/pepse/world/daynight/Sun.java
@@ -17,20 +17,22 @@ public class Sun {
     private static final int STARTINGPLACEPARAMX = 2;
     private static final int STARTINGPLACEPARAMY = 3;
 
-
-    public static GameObject create(Vector2 windowDimensions, float cycleLength){
+    public static GameObject create(Vector2 windowDimensions, float cycleLength) {
         Renderable renderable = new OvalRenderable(Color.YELLOW);
-        Vector2 starting_place = new Vector2
-        (windowDimensions.x()/STARTINGPLACEPARAMX, windowDimensions.y()/STARTINGPLACEPARAMY);
-        GameObject sun = new GameObject(starting_place, Vector2.ONES.mult(SUN_SIZE), renderable);
+        Vector2 starting_place = new Vector2(windowDimensions.x() / STARTINGPLACEPARAMX,
+                windowDimensions.y() / STARTINGPLACEPARAMY);
+        GameObject sun = new GameObject(starting_place, Vector2.ONES.mult(SUN_SIZE),
+                renderable);
         sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sun.setTag(SUN_TAG);
         Vector2 initialSunCenter = sun.getCenter();
-        Vector2 cycleCenter = new Vector2
-        (windowDimensions.x()/2, windowDimensions.y()*2/3);
-        new Transition<Float>(sun, (Float angle) -> sun.setCenter(initialSunCenter.subtract(cycleCenter)
-        .rotated(angle).add(cycleCenter)), 0f,FINALVAL, Transition.LINEAR_INTERPOLATOR_FLOAT,cycleLength
-        , TransitionType.TRANSITION_LOOP,null);
+        Vector2 cycleCenter = new Vector2(windowDimensions.x() / 2,
+                windowDimensions.y() * 2 / 3);
+        new Transition<Float>(sun,
+                (Float angle) -> sun.setCenter(initialSunCenter.subtract(cycleCenter)
+                        .rotated(angle).add(cycleCenter)),
+                0f, FINALVAL, Transition.LINEAR_INTERPOLATOR_FLOAT, cycleLength,
+                TransitionType.TRANSITION_LOOP, null);
         return sun;
     }
 }
--- Pepse/src/pepse/world/trees/Leaves.java
+++ Pepse/src/pepse/world/trees/Leaves.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 import danogl.collisions.GameObjectCollection;
 import danogl.collisions.Layer;
+import danogl.components.Transition;
+import danogl.components.Transition.TransitionType;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 import pepse.world.Block;
@@ -14,6 +16,10 @@ import pepse.world.Block;
 public class Leaves {
     private static final Color BASIC_COLOR = new Color(50, 200, 30);
     private static final Random random = new Random();
+    private static final float FINAL_ROTATING_ANGLE = 360f;
+    private static final float TRANSITION_LENGTH = 2;
+    private static final float MIN_WIDTH = 10f;
+    private static final float MAX_WIDTH = 60f;
     private List<Block> leaves;
 
     /**
@@ -37,11 +43,26 @@ public class Leaves {
             }
             y += Block.getSize();
         }
+        setTransitions();
     }
 
     public void addLeaves(GameObjectCollection collection) {
         for (Block block : leaves) {
             collection.addGameObject(block, Layer.FOREGROUND);
+        }
+    }
+
+    private void setTransitions() {
+        for (Block leaf : leaves) {
+            float leafHeight = leaf.getDimensions().y();
+            new Transition<Float>(leaf,
+                    (Float angle) -> leaf.renderer().setRenderableAngle(angle), 0f,
+                    FINAL_ROTATING_ANGLE, Transition.LINEAR_INTERPOLATOR_FLOAT,
+                    TRANSITION_LENGTH, TransitionType.TRANSITION_BACK_AND_FORTH, null);
+            new Transition<Float>(leaf,
+                    (Float width) -> leaf.setDimensions(new Vector2(width, leafHeight)),
+                    MIN_WIDTH, MAX_WIDTH, Transition.LINEAR_INTERPOLATOR_FLOAT,
+                    TRANSITION_LENGTH, TransitionType.TRANSITION_BACK_AND_FORTH, null);
         }
     }
 }
